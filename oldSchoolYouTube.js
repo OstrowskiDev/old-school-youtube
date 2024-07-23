46f3d1703794a33c262329b7756dae37b610e569
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2024-07-23 23:01:22 +0200

    Testing other approach for blocking YT Shorts.

--- oldSchoolYouTube.js
+++ oldSchoolYouTube.js
@@ -1,5 +1,29 @@
-const elementsWithSameId = document.querySelectorAll('[id="dismissable"]')
+console.log(`Reverting UI back to YouTube from good old days <3`)
+console.log(`Its 2010 again baby!!!!!!`)
 
-elementsWithSameId.forEach((element) => {
-  element.style.display = 'none'
-})
+setTimeout(() => {
+  const element = document.getElementById('dismissible')
+
+  if (element) {
+    console.log(`Found the element with id 'dismissible'`)
+    console.log(`The element is:`, element)
+    element.style.display = 'none'
+    element.setAttribute('style', 'display: none !important;')
+  } else {
+    console.log(`Element with id 'dismissible' not found`)
+  }
+}, 3000)
+
+// const element = document.getElementById('dismissible')
+
+// if (element) {
+//   console.log(`Found the element with id 'dismissible'`)
+//   console.log(`the element is:`, element)
+//   element.style.display = 'none'
+// }
+
+// const elementsWithSameId = document.querySelectorAll('[id="dismissible"]')
+
+// elementsWithSameId.forEach((element) => {
+//   element.style.display = 'none'
+// })
