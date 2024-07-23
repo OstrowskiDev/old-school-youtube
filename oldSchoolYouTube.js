2174f5aee62575adeb62e4558376d5402e5f93a2
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2024-07-23 22:15:46 +0200

    Added manifest.json. Added temporary app icon. Icon will be changed in final version.

--- oldSchoolYouTube.js
+++ oldSchoolYouTube.js
@@ -1,7 +1,5 @@
-// Select all elements with the ID "dismissable" using an attribute selector
 const elementsWithSameId = document.querySelectorAll('[id="dismissable"]')
 
-// Loop through the NodeList and apply changes
 elementsWithSameId.forEach((element) => {
   element.style.display = 'none'
 })
