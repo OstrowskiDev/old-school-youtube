f900784a0821e7b3af4887510899d9747952e466
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2024-07-24 21:45:30 +0200

    Adjusted code so it is able to deal with SPA behavior.

--- oldSchoolYouTube.js
+++ oldSchoolYouTube.js
@@ -1,18 +1,34 @@
-console.log(`[Old School YouTube] Attempting to revert UI back to YouTube from good old days...`)
+console.log(`[Old School YouTube] Extension initialized!`)
 
 function hideUnwantedContent() {
   const allElements = document.querySelectorAll('*')
+  console.log(`[Old School YouTube] Attempting to revert UI back to YouTube from good old days...`)
   disableShorts(allElements)
   disableMusicSection(allElements)
+  console.log(`[Old School YouTube] Welcome back to early YouTube days!`)
 }
 
 const intervalId = setInterval(() => {
   if (document.querySelector('#dismissible.ytd-rich-shelf-renderer')) {
     hideUnwantedContent()
-    console.log(`[Old School YouTube] Welcome back to early YouTube days!`)
     clearInterval(intervalId)
   }
 }, 100)
+
+let lastUrl = location.href
+
+new MutationObserver(() => {
+  const url = location.href
+  if (url !== lastUrl) {
+    lastUrl = url
+    if (url === 'https://www.youtube.com/' || url.startsWith('https://www.youtube.com/?')) {
+      console.log(`[Old School YouTube] Url changed, scanning for unwanted content...`)
+      hideUnwantedContent()
+    }
+  }
+}).observe(document, { subtree: true, childList: true })
+
+//!!!! need to add ulr sanitization!!!!
 
 function disableShorts(allElements) {
   const shortsElements = Array.from(allElements).filter((element) => element.id === 'dismissible' && element.classList.contains('ytd-rich-shelf-renderer'))
