103d4a664252ff9f809109cee0ebd884d8872f50
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2024-08-11 22:16:16 +0200

    Scrolling version basic code.

--- oldSchoolYouTube.js
+++ oldSchoolYouTube.js
@@ -1,33 +1,18 @@
-console.log(`[Old School YouTube] Extension initialized!`)
-
-function runOnPageLoad() {
-  const intervalId = setInterval(() => {
-    if (document.querySelector('#dismissible.ytd-rich-shelf-renderer')) {
-      console.log('[Old School YouTube]', document.querySelector('#dismissible.ytd-rich-shelf-renderer'))
-      hideUnwantedContent()
-      clearInterval(intervalId)
-    }
-  }, 500)
-}
+console.log(`[Old School YouTube]: Scrolling version 1.0 - Extension initialized!`)
 
 function hideUnwantedContent() {
-  const allElements = document.querySelectorAll('*')
   console.log(`[Old School YouTube] Attempting to revert UI back to YouTube from good old days...`)
-  disableShorts(allElements)
+  disableShorts()
   disableMusicSection(allElements)
   console.log(`[Old School YouTube] Welcome back to early YouTube days!`)
 }
 
-runOnPageLoad()
-
-function isValidYouTubeUrl(url) {
-  return url.hostname === 'www.youtube.com' && (url.pathname === '/' || url.pathname.startsWith('/?'))
-}
-
-let lastUrl = new URL(location.href)
-
-function disableShorts(allElements) {
-  const shortsElements = Array.from(allElements).filter((element) => element.id === 'dismissible' && element.classList.contains('ytd-rich-shelf-renderer'))
+function disableShorts() {
+  const shortsLikeElements = document.querySelectorAll('.ytd-rich-shelf-renderer')
+  if (shortsLikeElements) {
+    console.log(`[Old School YouTube] Shorts array length: ${shortsLikeElements.length}`)
+  }
+  const shortsElements = Array.from(shortsLikeElements).filter((element) => element.id === 'dismissible')
   console.log(`[Old School YouTube] Disabling Shorts...`)
 
   if (shortsElements.length > 0) {
@@ -40,12 +25,13 @@ function disableShorts(allElements) {
   }
 }
 
-function disableMusicSection(allElements) {
-  const musicElements = Array.from(allElements).filter((element) => element.id === 'dismissible' && element.classList.contains('ytd-statement-banner-renderer'))
+function disableMusicSection() {
+  const musicSectionLikeElements = document.querySelectorAll('.ytd-statement-banner-renderer')
+  const musicSectionElements = Array.from(musicSectionLikeElements).filter((element) => element.id === 'dismissible')
   console.log(`[Old School YouTube] Disabling Music section...`)
 
-  if (musicElements.length > 0) {
-    musicElements.forEach((music) => {
+  if (musicSectionElements.length > 0) {
+    musicSectionElements.forEach((music) => {
       music.style.display = 'none'
     })
     console.log(`[Old School YouTube] Music section disabled!`)
@@ -53,3 +39,4 @@ function disableMusicSection(allElements) {
     console.log(`[Old School YouTube] Error! Music section ont found.`)
   }
 }
+setTimeout(hideUnwantedContent, 1500)
