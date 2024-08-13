110c90d48035ae42ef4333f2158c71292d797942
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2024-08-13 20:19:20 +0200

    Adopted console logs for production.

--- oldSchoolYouTube.js
+++ oldSchoolYouTube.js
@@ -1,42 +1,31 @@
-console.log(`[Old School YouTube]: Scrolling version 1.0 - Extension initialized!`)
+console.log(`[Old School YouTube]: Extension initialized!`)
 
 function hideUnwantedContent() {
-  console.log(`[Old School YouTube] Attempting to revert UI back to YouTube from good old days...`)
   disableShorts()
   disableMusicSection(allElements)
-  console.log(`[Old School YouTube] Welcome back to early YouTube days!`)
 }
 
 function disableShorts() {
   const shortsLikeElements = document.querySelectorAll('.ytd-rich-shelf-renderer')
   if (shortsLikeElements) {
-    console.log(`[Old School YouTube] Shorts array length: ${shortsLikeElements.length}`)
   }
   const shortsElements = Array.from(shortsLikeElements).filter((element) => element.id === 'dismissible')
-  console.log(`[Old School YouTube] Disabling Shorts...`)
 
   if (shortsElements.length > 0) {
     shortsElements.forEach((shorts) => {
       shorts.style.display = 'none'
     })
-    console.log(`[Old School YouTube] Shorts disabled!`)
-  } else {
-    console.log(`[Old School YouTube] Error! Shorts ont found.`)
   }
 }
 
 function disableMusicSection() {
   const musicSectionLikeElements = document.querySelectorAll('.ytd-statement-banner-renderer')
   const musicSectionElements = Array.from(musicSectionLikeElements).filter((element) => element.id === 'dismissible')
-  console.log(`[Old School YouTube] Disabling Music section...`)
 
   if (musicSectionElements.length > 0) {
     musicSectionElements.forEach((music) => {
       music.style.display = 'none'
     })
-    console.log(`[Old School YouTube] Music section disabled!`)
-  } else {
-    console.log(`[Old School YouTube] Error! Music section ont found.`)
   }
 }
 
@@ -48,7 +37,6 @@ function scrollListener() {
     const distanceWithoutCheck = totalDistance - distanceChecked
 
     if (distanceWithoutCheck > 400) {
-      console.log(`[Old School YouTube] - distance threshold reached!`)
       disableShorts()
       distanceChecked = distanceChecked + 400
     }
