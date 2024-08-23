bb6c5b2db46864027c3ca278f61a38ede73834b2
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2024-08-23 22:37:20 +0200

    Added function to handle shorts on search page.

--- oldSchoolYouTube.js
+++ oldSchoolYouTube.js
@@ -1,10 +1,14 @@
 console.log(`[Old School YouTube]: Extension initialized!`)
 console.log(chrome.i18n.getMessage('extension-initialized'))
 
+//home page shorts containers hierarchy:
 let homePageRichContentContainerSelector = '#contents.ytd-rich-grid-renderer'
-// grid container, currently only has shorts section:
 let homePageContentGridSelector = '#content.ytd-rich-section-renderer'
 let homePageShortsSelector = 'ytd-rich-shelf-renderer'
+//search results page shorts containers hierarchy:
+let searchPageResultsContainerSelector = '#contents.ytd-item-section-renderer'
+let searchPageShortsSelector = 'ytd-reel-shelf-renderer:not([hidden="true"])'
+// others:
 let homePagePremiumMusicPromptSelector = 'ytd-statement-banner-renderer'
 let homePagePremiumAccountPromptSelector = 'need to add this'
 let videoPageShortsSelector = 'need to add this'
@@ -44,6 +48,7 @@ function hideElement(hide, element, onHideCallback = () => {}) {
   }
 }
 
+// home page has one shorts section that generates on page load
 function hideHomePageShorts(hide = true) {
   waitForElement(homePageRichContentContainerSelector, document.body)
     .then((wrapperElement1) => {
@@ -51,6 +56,22 @@ function hideHomePageShorts(hide = true) {
     })
     .then((wrapperElement2) => {
       return waitForElement(homePageShortsSelector, wrapperElement2)
+    })
+    .then((element) => {
+      if (element != null) {
+        hideElement(hide, element)
+      }
+    })
+    .catch((error) => {
+      console.error(`[Old School YouTube]: Error in hideHomePageShorts: ${error}`)
+    })
+}
+
+// serach page has multiple shorts sections, they generate while user is scrolling down
+function hideSearchPageShorts(hide = true) {
+  waitForElement(searchPageResultsContainerSelector, document.body)
+    .then((wrapperElement1) => {
+      return waitForElement(searchPageShortsSelector, wrapperElement1, { childList: true, subtree: false })
     })
     .then((element) => {
       if (element != null) {
